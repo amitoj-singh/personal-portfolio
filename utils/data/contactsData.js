2c128024e60dbb16b2b7c57b6701ceaec4b0fc1f
Amitoj <amitoj.98@gmail.com>
2026-03-01 14:23:03 +0530

    protfolio upgraded to a new template using nextJS

---
+++ utils/data/contactsData.js
@@ -0,0 +1,9 @@
+export const contactsData = {
+    email: 'amitoj.98@gmail.com',
+    phone: '+91 902 3434 518',
+    address: 'Patiala, Punjab, India - 147001',
+    github: 'https://github.com/amitoj-singh/',
+    linkedIn: 'https://www.linkedin.com/in/amitoj-singh-762365168/',
+    twitter: 'https://x.com/amitoj__singh',
+    devUsername: "NA"
+}
---
+++ utils/data/educations.js
@@ -0,0 +1,14 @@
+export const educations = [
+  {
+    id: 1,
+    title: "B.Tech in Computer Science",
+    duration: "2017 - 2021",
+    institution: "Punjabi University Patiala",
+  },
+  {
+    id: 2,
+    title: "Higher School",
+    duration: "2014 - 2016",
+    institution: "Kendriya Vidayalaya, Patiala",
+  }
+]
